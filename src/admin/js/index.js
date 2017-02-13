a73f5b79cbb47afc4ec7364a00af900c27adbc0c
idsbllp <idsbllp@gmail.com>
2017-02-13 17:11:26 +0800

    add editor css file

--- src/admin/js/index.js
+++ src/admin/js/index.js
@@ -2,14 +2,14 @@
 import Vue from "vue";
 
 import app from '../template/app.vue';
-Vue.config.debug = true;//开启错误提示
+Vue.config.debug = true; //开启错误提示
 
 new Vue(app);
 
 import "../fonts/Arsenal-Regular.ttf";
-import "../css/index.css"
+import "../css/index.css";
 
 // online editor. see: http://lanfly.github.io/laneditor/doc/
-import "../LanEditor/LanEditor.css"
-import "../../../node_modules/highlight.js/styles/agate.css"
+import "../css/LanEditor.css";
+import "../../../node_modules/highlight.js/styles/agate.css";
 
